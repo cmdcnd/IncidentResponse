58f0ea82078ee3296bc30488a65527bc769a8b69
drichmond-github <86841997+drichmond-github@users.noreply.github.com>
2023-01-25 14:26:47 -0800

    Update install-wazuh.bat

--- 4.Eradication/4.08_Deploy_or_evaluate_host_sensors/scripts/install-wazuh.bat
+++ 4.Eradication/4.08_Deploy_or_evaluate_host_sensors/scripts/install-wazuh.bat
@@ -1,2 +1,2 @@
-c:\Tools\wazuh-agent-3.13.1-1.msi /q WAZUH_MANAGER="x.x.x.x" WAZUH_REGISTRATION_SERVER="x.x.x.x"
+c:\SoftwareTools\wazuh-agent-3.13.1-1.msi /q WAZUH_MANAGER="x.x.x.x" WAZUH_REGISTRATION_SERVER="x.x.x.x"
 net start "Wazuh"
